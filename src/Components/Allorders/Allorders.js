981058e5dc3d2a24da143e21ccdbf24b8ae1f473
Zayn Yusuf <yusue786@gmail.com>
2022-05-08 14:29:01 +0600

    load all user

--- src/Components/Allorders/Allorders.js
+++ src/Components/Allorders/Allorders.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Allorders = () => {
+    const [orders,setOrders]=useState([])
+    const[user]=useAuthState(auth)
+    useEffect(()=>{
+        const email=user?.email
+        const url=`http://localhost:2000/order?email=${email}`
+        fetch(url)
+        .then(response=>response.json())
+        .then(data=>setOrders(data))
+    },[user])
     return (
         <div>
-            <h1>All order is hare</h1>
+            <h1>All order is:{orders.length}</h1>
         </div>
     );
 };
